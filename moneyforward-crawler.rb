a78a9ae7dc6931ca43e5531edd8520e76e3cbdf0
Kouhei Suzuki <kouhei.szk@gmail.com>
2015-08-03 00:45:26 +0900

    Cookieセットおかしい

--- moneyforward-crawler.rb
+++ moneyforward-crawler.rb
@@ -1,55 +1,45 @@
 require 'pry-byebug'
 require 'nokogiri'
-require 'capybara'
-require 'capybara/poltergeist'
+require 'mechanize'
 require 'yaml'
 
 class MoneyforwardCrawler
   def initialize
     @config = YAML.load_file('config.yml')
-    binding.pry
   end
 
   def retrieve
-    Capybara.register_driver :poltergeist do |app|
-      Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 1000, phantomjs_options: ['--ignore-ssl-errors=true', '--ssl-protocol=any'])
+    agent = Mechanize.new do |agent|
+      agent.log = Logger.new($stdout)
+      agent.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12'
     end
-    Capybara.javascript_driver = :poltergeist
-    Capybara.current_driver = :poltergeist
-    Capybara.default_selector = :css
 
-    session = Capybara::Session.new(:poltergeist)
-    session.driver.headers = { 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12' }
-    binding.pry
-    session.instance_eval do
-      visit 'https://moneyforward.com/users/sign_in'
+    site_url = 'https://moneyforward.com'
+    uri = URI.parse(site_url)
+
+    # ログイン
+    agent.get('https://moneyforward.com/users/sign_in') do |page|
+      puts page.title
+      page.form_with(id: 'new_sign_in_session_service') do |form|
+        form.field_with(name: 'sign_in_session_service[email]').value = @config['email']
+        form.field_with(name: 'sign_in_session_service[password]').value = @config['password']
+      end.submit
+    end
+    return unless agent.page.uri.to_s == 'https://moneyforward.com/'
+
+    # 家計簿に移動
+
+    cookie = Mechanize::Cookie.new('cf_last_fetch_from_date', uri.host, { value: '2014%2F09%2F01', domain: uri.host, path: '/' })
+    agent.cookie_jar.add(uri.host, cookie)
+
+    agent.get('https://moneyforward.com/cf') do |page|
+
+      puts page.title
+      html = Nokogiri::HTML(page.body)
+      rows = html.search('table#cf-detail-table > tbody > tr')
+
       binding.pry
-      fill_in 'sign_in_session_service[email]', with: @config['email']
-      fill_in 'sign_in_session_service[password]', with: @config['password']
-      click_on 'ログイン'
     end
-
-    # # ログイン
-    # agent.get('https://moneyforward.com/users/sign_in') do |page|
-    #   puts page.title
-    #   page.form_with(id: 'new_sign_in_session_service') do |form|
-    #     form.field_with(name: 'sign_in_session_service[email]').value = @config['email']
-    #     form.field_with(name: 'sign_in_session_service[password]').value = @config['password']
-    #   end.submit
-    # end
-    # return unless agent.page.uri.to_s == 'https://moneyforward.com/'
-    #
-    # cookie = Mechanize::Cookie.new('cf_last_fetch_from_date', agent.page.uri.host, { value: '2014%2f08%2f02', domain: agent.page.uri.host, path: '/' })
-    # agent.cookie_jar.add(agent.page.uri.host, cookie)
-    #
-    # # 家計簿に移動
-    # agent.get('https://moneyforward.com/cf') do |page|
-    #   puts page.title
-    #   html = Nokogiri::HTML(page.body)
-    #   rows = html.search('table#cf-detail-table > tbody > tr')
-    #
-    #   binding.pry
-    # end
 
     puts 'Finish'
   end
