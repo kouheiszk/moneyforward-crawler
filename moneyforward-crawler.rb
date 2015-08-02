722ba4e5da6963cb8046e1b85ef3558a08c75ed6
Kouhei Suzuki <kouhei.szk@gmail.com>
2015-08-02 12:24:41 +0900

    Add cookie to change month

--- moneyforward-crawler.rb
+++ moneyforward-crawler.rb
@@ -1,5 +1,6 @@
 require 'pry-byebug'
 require 'mechanize'
+require 'nokogiri'
 require 'yaml'
 
 class MoneyforwardCrawler
@@ -9,10 +10,12 @@ class MoneyforwardCrawler
 
   def retrieve
     agent = Mechanize.new
+    agent.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12'
     # agent.agent.http.debug_output = $stderr
 
     # ログイン
     agent.get('https://moneyforward.com/users/sign_in') do |page|
+      puts page.title
       page.form_with(id: 'new_sign_in_session_service') do |form|
         form.field_with(name: 'sign_in_session_service[email]').value = @config['email']
         form.field_with(name: 'sign_in_session_service[password]').value = @config['password']
@@ -20,11 +23,14 @@ class MoneyforwardCrawler
     end
     return unless agent.page.uri.to_s == 'https://moneyforward.com/'
 
+    cookie = Mechanize::Cookie.new('cf_last_fetch_from_date', agent.page.uri.host, { value: '2014%2f08%2f02', domain: agent.page.uri.host, path: '/' })
+    agent.cookie_jar.add(agent.page.uri.host, cookie)
+
     # 家計簿に移動
     agent.get('https://moneyforward.com/cf') do |page|
+      puts page.title
       html = Nokogiri::HTML(page.body)
-      year_node = html.search('div#calendar')
-      month_node = html.search('div#calendar')
+      rows = html.search('table#cf-detail-table > tbody > tr')
 
       binding.pry
     end
